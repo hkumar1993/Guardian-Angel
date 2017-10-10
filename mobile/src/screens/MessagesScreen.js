7caaf8e401ab2a7c81bdcbd095a67be5ae2e3b18
Clay Shaw <Claydshaw@gmail.com>
2017-10-10 10:56:17 -0700

    Set up navigation

---
+++ mobile/src/screens/MessagesScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+
+const Root = styled.View`
+
+`;
+
+const T = styled.Text`
+
+`;
+
+class MessagesScreen extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Root>
+        <T>Messages</T>
+      </Root>
+    )
+  }
+}
+
+export default MessagesScreen;
