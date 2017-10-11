d129318ae3607acee0372545cc6afa2b18c37398
Harsh <harsh@hkumar.me>
2017-10-11 09:07:32 -0700

    Create Need Show page

---
+++ mobile/src/components/NeedShow/GuardianInfor.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import Touchable from '@appandflow/touchable';
+
+import { fakeAvatar } from '../../utils/constants';
+import { withNavigation } from 'react-navigation';
+
+const AVATAR_SIZE = 30;
+const AVATAR_RADIUS = AVATAR_SIZE / 2;
+
+const Root = styled.View`
+  height: 50;
+  flexDirection: row;
+  alignItems: center;
+  justifyContent: space-between;
+`;
+
+const AvatarContainer = styled.View`
+  flex: 0.2;
+  alignItems: center;
+  justifyContent: center;
+`;
+
+  const Avatar = styled.Image`
+  height: ${ AVATAR_SIZE } ;
+  width: ${ AVATAR_SIZE } ;
+  borderWidth: 2px;
+  borderColor: ${props => props.theme.LIGHT_BLUE};
+  borderRadius: ${ AVATAR_RADIUS };
+`;
+
+//
+const MetaContainer = styled.View`
+  flex: 1;
+  alignSelf: stretch;
+`;
+
+const MetaTopContainer = styled.View`
+  flex: 0.8;
+  flexDirection: row;
+  alignItems: center;
+  justifyContent: flex-start;
+`;
+
+const Button = styled(Touchable).attrs({
+  feedback: 'opacity'
+})`
+  flex: 1;
+  flexDirection: row;
+  alignItems: center;
+  justifyContent: flex-start;
+  marginLeft: 5px;
+`;
+
+const ButtonText = styled.Text`
+  alignSelf: stretch;
+  fontSize: 16;
+  fontWeight: 700;
+  color: ${ props => props.theme.LIGHT_BLUE }
+`;
+// will come from back end later
+// const username = 'itsClay';
+// const firstName = 'Clay';
+// const lastName = 'Shaw';
+// const createdAt = '1 day ago';
+// const avatar = fakeAvatar;
+
+function NeedCardHeader(props) {
+  title = props.title
+  avatar = props.avatar
+  navigate = props.navigation.navigate
+  return (
+    <Root>
+     <MetaTopContainer>
+       <Button onPress={() => navigate('Need', {need: props.need})}>
+         <ButtonText>
+           {title}
+         </ButtonText>
+       </Button>
+     </MetaTopContainer>
+     <AvatarContainer>
+       <Avatar source={{ uri: avatar || fakeAvatar }} />
+     </AvatarContainer>
+    </Root>
+  )
+}
+//  <MetaText style={{ marginLeft: 5 }} >
+//    @{username}
+//  </MetaText>
+// <MetaBottomContainer>
+//   <MetaText>
+//     { distanceInWordsToNow(createdAt) } ago
+//   </MetaText>
+// </MetaBottomContainer>
+export default withNavigation(NeedCardHeader);
---
+++ mobile/src/components/NeedShow/NeedShow.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+import { withNavigation } from 'react-navigation';
+import NeedCardBottom from '../NeedCard/NeedCardBottom';
+
+const Root = styled.View`
+  height: 100%;
+  padding: 7px;
+  backgroundColor: white;
+  width: 100%;
+  alignSelf: center;
+  flexDirection: column;
+  justifyContent: space-between;
+`;
+
+
+const MapContainer = styled.View`
+  height: 40%;
+  backgroundColor: ${props => props.theme.TAG_BLUE}
+`;
+
+const NeedContentContainer = styled.View`
+padding: 10px 20px 10px 10px;
+`;
+
+const NeedTitle = styled.Text`
+  fontSize: 20;
+  textAlign: left;
+  fontWeight: 700;
+  marginBottom: 10px;
+  color: ${ props => props.theme.LIGHT_BLUE}
+`;
+
+const NeedDescription = styled.Text`
+  fontSize: 16;
+  textAlign: left;
+  fontWeight: 500;
+  color: ${ props => props.theme.DARK_GREY}
+`;
+
+
+
+// const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut'
+
+class NeedShow extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = { need: this.props.navigation.state.params.need }
+    console.log(this.state.need);
+  }
+
+  render(){
+    const need = this.state.need
+    return (
+      <Root>
+        <MapContainer>
+
+        </MapContainer>
+        <NeedContentContainer >
+          <NeedTitle>
+            { need.title }
+          </NeedTitle>
+          <NeedDescription>
+            { need.description }
+          </NeedDescription>
+        </NeedContentContainer>
+        <NeedCardBottom />
+      </Root>
+    );
+  }
+};
+
+export default withNavigation(NeedShow);
