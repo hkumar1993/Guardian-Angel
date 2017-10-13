800f485c9dfca4e4c64c9495c6894b9517e64876
Clay Shaw <Claydshaw@gmail.com>
2017-10-13 10:39:20 -0700

    Move graphql server info to index

--- Server/src/config/middlewares.js
+++ Server/src/config/middlewares.js
@@ -1,16 +1,6 @@
 import bodyParser from "body-parser";
-import { graphiqlExpress, graphqlExpress } from "apollo-server-express";
-import { makeExecutableSchema } from "graphql-tools";
 
-import constants from "./constants";
-import typeDefs from "../graphql/schema";
-import resolvers from "../graphql/resolvers";
 import { decodeToken } from "../services/auth";
-
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers
-});
 
 async function auth(req, res, next) {
   try {
@@ -30,19 +20,5 @@ async function auth(req, res, next) {
 export default app => {
   app.use(bodyParser.json());
   app.use(auth);
-  app.use(
-    "/graphiql",
-    graphiqlExpress({
-      endpointURL: constants.GRAPHQL_PATH
-    })
-  );
-  app.use(
-    constants.GRAPHQL_PATH,
-    graphqlExpress(req => ({
-      schema,
-      context: {
-        user: req.user
-      }
-    }))
-  );
+
 };
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-console */
 
 import express from "express";
+
 import { createServer } from "http";
+import { graphiqlExpress, graphqlExpress } from "apollo-server-express";
+import { makeExecutableSchema } from "graphql-tools";
+import { SubscriptionServer } from 'subscriptions-transport-ws'
+import { execute, subscribe } from 'graphql'
 
 import "./config/db";
+import typeDefs from "./graphql/schema";
+import resolvers from "./graphql/resolvers";
 import constants from "./config/constants";
 import middlewares from "./config/middlewares";
 import mocks from "./mocks";
@@ -12,6 +19,29 @@ const app = express();
 
 middlewares(app);
 
+app.use(
+  "/graphiql",
+  graphiqlExpress({
+    endpointURL: constants.GRAPHQL_PATH,
+    SubscriptionsEndpoint: `ws:/localhost:${constants.PORT}${constants.SUBSCRIPTIONS_PATH}`
+  })
+);
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+app.use(
+  constants.GRAPHQL_PATH,
+  graphqlExpress(req => ({
+    schema,
+    context: {
+      user: req.user
+    }
+  })),
+);
+
 const graphQLServer = createServer(app);
 
 mocks().then(() => {
@@ -19,6 +49,14 @@ mocks().then(() => {
     if (err) {
       console.error(err);
     } else {
+      new SubscriptionServer({
+        schema,
+        execute,
+        subscribe
+      }, {
+        server: graphQLServer,
+        path: constants.SUBSCRIPTIONS_PATH
+      })
       console.log(`App listen to port: ${constants.PORT}`);
     }
   });
