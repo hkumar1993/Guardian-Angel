3d25801d69355fee6ecd6e5e72f0541e8e61c548
Harsh <harsh@hkumar.me>
2017-10-13 09:53:48 -0700

    Update package for tables

---
+++ mobile/src/components/Dashboard/RequestedNeedScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+import TableView from './Table'
+
+const Root = styled.View`
+  width: 100%;
+  alignItems: center;
+  paddingTop: 20;
+`;
+
+const T = styled.Text`
+
+`;
+
+class RequestedNeedScreen extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Root>
+        <TableView  />
+      </Root>
+    )
+  }
+}
+
+export default RequestedNeedScreen;
