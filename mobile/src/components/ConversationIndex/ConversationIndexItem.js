297590ed119188f87e67cd5465749dbd3a0d7244
Clay Shaw <Claydshaw@gmail.com>
2017-10-14 18:39:56 -0700

    Initial commit

---
+++ mobile/src/components/ConversationIndex/ConversationIndexItem.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import styled from 'styled-components/native';
+import { withNavigation } from 'react-navigation';
+import Touchable from '@appandflow/touchable';
+import { graphql, withApollo, compose } from 'react-apollo';
+import { connect } from 'react-redux';
+
+import ConversationIndexLeft from './ConversationIndexLeft';
+import ConversationIndexContent from './ConversationIndexContent';
+import ConversationIndexHeader from './ConversationIndexHeader';
+
+
+const Root = styled(Touchable).attrs({
+  feedback: 'opacity'
+})`
+  minHeight: 80;
+  padding: 5px;
+  backgroundColor: white;
+  width: 100%;
+  shadowColor: ${ props => props.theme.LIGHT_BLUE };
+  shadowOffset: 0px 5px;
+  shadowRadius: 2;
+  shadowOpacity: 0.1;
+`;
+
+const ConversationContentContainer = styled.View`
+  flex: 1;
+  flexDirection: row;
+`;
+
+const AvatarContainer = styled.View`
+  width: 50;
+`;
+
+const ConversationTextContainer = styled.View`
+  marginLeft: 5;
+`;
+
+
+class ConversationIndexItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.nameToDisplay = this.nameToDisplay.bind(this)
+  }
+
+  nameToDisplay() {
+    const currentUser = this.props.user.info
+    const recipient = this.props.item.recipient
+    const author = this.props.item.author
+    if (currentUser.username === author.username) {
+      return recipient.firstName + ' ' + recipient.lastName
+    }
+    return author.firstName + ' ' + author.lastName
+  }
+
+  render() {
+    const name = this.nameToDisplay();
+
+    console.log('Name to display: ', name);
+    console.log("THIS ConversationIndexItem", this);
+    console.log('PROPS ConversationIndexItem', this.props);
+    return (
+      <Root onPress={() => navigate('Conversation')} >
+        <ConversationContentContainer>
+          <AvatarContainer>
+            <ConversationIndexLeft />
+          </AvatarContainer>
+          <ConversationTextContainer >
+            <ConversationIndexHeader name={name} style={{ width: 20 }}/>
+            <ConversationIndexContent/>
+          </ConversationTextContainer>
+        </ConversationContentContainer>
+      </Root>
+    );
+  }
+};
+
+export default withNavigation(ConversationIndexItem);
