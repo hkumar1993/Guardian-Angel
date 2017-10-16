ca2c5d2b3ddf58be138171852e558ebc21babfd3
Harsh <harsh@hkumar.me>
2017-10-15 17:37:01 -0700

    Add new dependencies for dahsboard

--- mobile/src/components/Dashboard/PostedNeedScreen.js
+++ mobile/src/components/Dashboard/PostedNeedScreen.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
 import TableView from './Table'
+import Touchable from '@appandflow/touchable';
+import { withNavigation } from 'react-navigation'
+import Loading from '../Loading'
+
+import { graphql, compose, withApollo } from 'react-apollo';
+import { connect } from 'react-redux';
+
+import GET_USER_NEEDS_QUERY from '../../graphql/queries/getUserNeeds';
+import { GET_NEED_REQUESTS, GET_USER_REQUESTS } from '../../graphql/queries/getRequests'
+import { NEED_REQUEST_ADDED, NEED_REQUEST_DELETED } from '../../graphql/subscriptions/needRequestSubscriptions'
+
 
 const Root = styled.View`
   width: 100%;
